cd3a8c8fe4eacaf0d4d5d7e65e3967ce72961e92
Kellen Donohue <kellend@cs.washington.edu>
2011-10-11 19:43:37 -0700

    Initial add of project 2

---
+++ 326/Project 2/MazeRunner.java
@@ -0,0 +1,16 @@
+import java.io.PrintWriter;
+
+/**
+ * Abstract base class for MazeRunners. It has a solveMaze method that, given a
+ * maze, will attempt to find a path from the start to the goal in the maze.
+ */
+public interface MazeRunner {
+  /**
+   * Attempts to find a path from the start to the donut node of the maze given
+   * in <code>maze</code>.
+   * 
+   * @param maze The maze to solve.
+   * @param writer The PrintWriter on which to output the maze solution.
+   */
+  public void solveMaze(Maze maze, PrintWriter writer);
+}
